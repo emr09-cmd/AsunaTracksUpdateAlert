ea36ebff0b66a1ac53f0fbd6eb53193bbe2be05d
emr09 <emirpdalan@icloud.com>
2026-07-17 16:08:07 +0200

    Update

--- Sources/AsunaTracksUpdateAlert/AsunaTracksUpdateAlert.swift
+++ Sources/AsunaTracksUpdateAlert/AsunaTracksUpdateAlert.swift
@@ -1,2 +1,35 @@
 // The Swift Programming Language
 // https://docs.swift.org/swift-book
+
+import SwiftUI
+
+@available(iOS 15.0, *)
+public struct AsunaTracksUpdateAlert: View {
+
+    private let title: String
+    private let message: String
+
+    @State private var showAlert = false
+
+    public init(
+        title: String = "Hello world",
+        message: String = "AsunaTracksUpdateAlert is working!"
+    ) {
+        self.title = title
+        self.message = message
+    }
+
+    public var body: some View {
+        Color.clear
+            .onAppear {
+                showAlert = true
+            }
+            .alert(title, isPresented: $showAlert) {
+                Button("OK") {
+                    showAlert = false
+                }
+            } message: {
+                Text(message)
+            }
+    }
+}
